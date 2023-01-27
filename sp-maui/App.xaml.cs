2bc135fedbcce2618ac60ebdc7dd7b8b8b898171
Marc Manuel <marc_manuel@hotmail.com>
2023-01-26 19:18:40 -0500

    Add project files.

---
+++ sp-maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace sp_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ sp-maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace sp_maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
